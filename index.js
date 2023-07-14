8b653a6ef85771ee9b71e8b1ad4458e60d14abdd
brewchetta <chett.tiller@gmail.com>
2023-07-14 11:51:46 -0400

    jokes for days

--- index.js
+++ index.js
@@ -25,6 +25,11 @@
 // WE WANT JSON
 // Java Script Object Notation
 
+// 1. Make an initial fetch request to the Dad Jokes API and add a new paragraph tag to the `dad-jokes-container` with the new joke.
+
+//   - HINT: Be sure to look at the data you get back so you know how to get the joke inside the response...
+const dadJokesContainer = document.querySelector("#dad-jokes-container")
+
 const OPTIONS = {
     headers: {
         'Accept': 'application/json'
@@ -34,14 +39,25 @@ const OPTIONS = {
 fetch("https://icanhazdadjoke.com/", OPTIONS)
 .then( response => response.json() )
 .then( data => {
-    console.log(data)
+    // console.log(data)
     const newPWithTheDadJoke = document.createElement("p")
     newPWithTheDadJoke.textContent = data.joke
-    const dadJokesContainer = document.querySelector("#dad-jokes-container")
     dadJokesContainer.append(newPWithTheDadJoke)
 })
 
-// 1. Make an initial fetch request to the Dad Jokes API and add a new paragraph tag to the `dad-jokes-container` with the new joke.
+// 3. Start up json-server with `json-sever --watch db.json`. If you aren't able then attempt to install the json-server (installation depends on your OS). You can now fetch jokes from `http://localhost:3000/jokes`. Fetch the array of jokes, iterate through the array, and create a new paragraph tag for each joke just like above.
 
-//   - HINT: Be sure to look at the data you get back so you know how to get the joke inside the response...
+//   - HINT: Once again make sure you look at the data you get back, it WILL be different from the dad jokes API
 
+function addJoke(jokeObject) {
+    const newPTagThatHasTheJoke = document.createElement('p')
+    newPTagThatHasTheJoke.textContent = jokeObject.content
+    dadJokesContainer.append(newPTagThatHasTheJoke)
+}
+
+fetch('http://localhost:3000/jokes')
+.then( response => response.json() )
+.then( jokesArray => {
+    console.log(jokesArray) 
+    jokesArray.forEach(addJoke)
+})
